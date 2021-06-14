caf25c3e1f590e88811410cb60c690bae98c642c
Andrei Freitas <westerlanio@gmail.com>
2021-06-14 20:20:20 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position p = new Position(5, 1);
+		System.out.println(p);
+
+	}
+
+}
---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+	private int row;
+	private int column;
+
+	public Position() {
+
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public String toString() {
+		return row + ", " + column;
+	}
+}
