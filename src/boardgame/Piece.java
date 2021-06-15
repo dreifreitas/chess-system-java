dbc93aa7898f6f9945a09eacd49e027439a089e5
Andrei Freitas <westerlanio@gmail.com>
2021-06-14 21:01:25 -0300

    starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,24 @@
+package boardgame;
+
+public abstract class Piece {
+	protected Position position;
+	private Board board;
+
+	public Piece() {
+
+	}
+
+	public Piece(Board board) {
+		this.board = board;
+
+	}
+
+	
+	protected Board getBoard() {
+		return board;
+	}
+
+	public abstract boolean possibleMoves();
+
+	
+}
